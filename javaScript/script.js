6970e181ee23a3c8dc21e956e5caf99f882f1824
Pushapak <pp281997@gmail.com>
2021-10-05 12:42:11 +0530

    initial

---
+++ javaScript/script.js
@@ -0,0 +1,65 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+
+
+//  show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//  hide loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Get Quote from API
+async function getQuote() {
+    loading();
+    const proxyUrl = 'https://frozen-springs-13529.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // if author is blank
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // reduce font size
+        if (data.quoteText.length > 50) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+
+        quoteText.innerText = data.quoteText;
+        complete();
+
+    } catch (error) {
+        getQuote();
+    }
+}
+// tweet quote function
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+// Event listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote);
+// on Load
+getQuote();
+
+    
